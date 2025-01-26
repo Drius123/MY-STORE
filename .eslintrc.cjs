3f34c4322e97396fb02fa1d869c3bc3e0c34ea8d
Drius123 <adovgulev111@gmail.com>
2025-01-26 17:12:50 +0300

    feat: add lora

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,33 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
-}
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
+import Nav from "../../ui/Nav/Nav";
 import styles from "./Header.module.scss";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <p>HEadeer</p>
+      <Nav />
     </div>
   );
 }
